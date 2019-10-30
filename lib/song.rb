2b3269f9a94952887c2cb3290ea5d1244457cf5e
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-30 23:52:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,7 @@ class Song
   def self.genre
     @@genre.uniq
   end
- attr_accessor :name
- attr_accessor :artist
- attr_accessor :genre
-
+ def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end 
 end
